1537cee5085701e665173d6fade87a26b226416f
Philipp Wieck <wieckphilipp@gmail.com>
2026-09-21 21:07:04 +0200

    =||=

--- configuration.nix
+++ configuration.nix
@@ -366,12 +366,11 @@ networking.wireless = {
     "-w /etc/sudoers -p wa -k sudoers"
     "-a exit,always -F arch=b64 -S open,openat -F dir=/etc -F success=1 -k etc_access"
   ];
-
-  security.auditd.extraConfigFile = ''
-    max_log_file = 50
-    max_log_file_action = ROTATE
-    num_logs = 5
-  '';
+  services.auditd.settings = {
+    max_log_file = 50;
+    max_log_file_action = "ROTATE";
+    num_logs = 5;
+  };
 
   services.fail2ban = {
     enable = true;
